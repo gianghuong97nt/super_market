3cc57af147d6740f8b5199a7f9a2c31f86b7ac33
gianghuong97nt <gianghuong97nt@gmail.com>
2019-04-25 13:03:50 +0700

    them

--- manage/resources/views/personalInfo/info.blade.php
+++ manage/resources/views/personalInfo/info.blade.php
@@ -1,63 +1,105 @@
 @extends('layouts.glance')
 
-
+@section('link')
+    <link href="{{asset('asset/css/form_info_personal.css')}}" rel='stylesheet' type='text/css' />
+@endsection
 @section('content')
-    <div class="forms">
-        <h2 class="title1">Personal Information</h2>
-        <button type="submit" class="btn btn-primary submit2">Update</button>
-        <div class="form-grids row widget-shadow" data-example-id="basic-forms">
-            <div class="form-body">
-                <form>
-                    <div class="form-group">
-                            <label for="userID" class="inline">User ID</label>
-                            <input  type="text" class="form-control inline" id="userID" value="{{'112'}}">
-                    </div>
-                    <div class="form-group">
-                        <label for="username" class="inline">Username</label>
-                        <input  type="text" class="form-control inline" id="username" value="{{'Giang'}}">
-                    </div>
-                    <div class="form-group">
-                        <label for="email" class="inline">Email address</label>
-                        <input type="email" class="form-control inline" id="email" value="{{'giang@gmail.com'}}">
-                    </div>
-                    <div class="form-group">
-                        <label for="phone" class="inline">Phone Number</label>
-                        <input type="text" class="form-control inline" id="phone" value="{{'0969344759'}}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="address" class="inline">Birthday</label>
-                        <input type="date" class="form-control inline" id="birthday" value="{{'04/14/1997'}}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="password" class="inline">Password</label>
-                        <input type="password" class="form-control inline" id="password" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                        <label for="repassword" class="inline">Confirm Password</label>
-                        <input type="password" class="form-control inline" id="repassword" placeholder="Confirm Password">
-                    </div>
-                    <div class="form-group">
-                        <label for="address" class="inline">Address</label>
-                        <input type="text" class="form-control inline" id="address" value="{{'Thái Bình'}}">
-                    </div>
-                    <div class="form-group">
-                        <label class="inline">Gender</label>
-                        <select class="inline">
-                                <option>Male</option>
-                                <option>Female</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="inline "><label for="avatar">Avatar</label></div>
-                        <div class="inline ">
-                            <img src="{{'asset/images/cuc.png'}}" style="height: 25%; width: 25%">
+    <div class="forms tables">
+        <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-4 col-lg-2 btn-btn">
+                <h2 class="title1">Personal Information</h2>
+                <button type="button" class="btn btn-success btn-2">Save</button>
+                <a href="{{'http://localhost:8005'}}"><button type="button" class="btn btn-success btn-2">Back</button></a>
+            </div>
+        </div>
+        <div class="panel-body bs-example widget-shadow" data-example-id="basic-forms">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="form-body">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info" >User ID</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" class="form-control" id="userID" disabled value="{{'112'}}"/>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">Tên</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" class="form-control" id="username" value="{{'Giang'}}"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">Email</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" class="form-control" id="email" value="{{'giang@gmail.com'}}"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">SĐT</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" class="form-control" id="phone" value="{{'0969344759'}}"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">Ngày sinh</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input type="date" class="form-control" id="birthday" value="{{'04/14/1997'}}"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">Mật khẩu</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input type="password" class="form-control" id="password" placeholder="Password"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">Xác nhận MK</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input type="password" class="form-control" id="repassword" placeholder="Confirm Password"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">Địa chỉ</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" class="form-control" id="address" value="{{'Thái Bình'}}"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">Giới tính</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <select class="form-control" id="gender">
+                                <option>Male</option>
+                                <option>Female</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                            <label class="form-control label-info">Ảnh</label>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <img src="{{'asset/images/cuc.png'}}" id="avatar">
+                        </div>
 
-                </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
