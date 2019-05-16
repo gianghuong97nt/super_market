1074e21f6e47db871fbb7bf360215ce20399a1d7
gianghuong97nt <gianghuong97nt@gmail.com>
2019-05-16 11:22:23 +0700

    them

--- manage/resources/views/product/search.blade.php
+++ manage/resources/views/product/search.blade.php
@@ -1,24 +1,42 @@
+<div class="row" style="margin: 0">
+    <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12">
+        <label class="form-control label-info">Số trang hiển thị</label>
+    </div>
+    <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+        <select id="page_size">
+            <option value="5" style="text-align: center">5</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+        </select>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <div class="mt-15 panel-footer-search pull-right pagination_product"  style="margin: 0 ; float: right" id="loadProduct">
+            {!!Paging::show($paging,0)!!}
+        </div>
+    </div>
+</div>
+
 <table class="table table-bordered tbl_product">
     <thead>
         <tr class="product">
-            <th>Mã </th>
-            <th>Tên SP </th>
-            <th>Danh mục</th>
-            <th>Hãng</th>
-            <th>Nhà CC</th>
-            <th>SL </th>
-            <th>Màu</th>
-            <th>Cỡ</th>
-            <th>Giá nhập</th>
-            <th>Giá bán</th>
-            <th>Note</th>
-            <th class="action"><a class="btn btn-primary" id="btn-add-row-1" href="{{url('/product/detail')}}"><i class="fa fa-plus" aria-hidden="true"></i></a></th>
+            <th class="tbl_id">Mã </th>
+            <th class="tbl_name">Tên SP </th>
+            <th class="tbl_category">Danh mục</th>
+            <th class="tbl_brand">Hãng</th>
+            <th class="tbl_supplier">Nhà CC</th>
+            <th class="tbl_quantity">SL </th>
+            <th class="tbl_color">Màu</th>
+            <th class="tbl_size">Cỡ</th>
+            <th class="tbl_price_core">Giá nhập</th>
+            <th class="tbl_price_sale">Giá bán</th>
+            <th class="tbl_note">Note</th>
+            <th class="action tbl_action"><a class="btn btn-primary" id="btn-add-row-1" href="{{url('/product/detail')}}"><i class="fa fa-plus" aria-hidden="true"></i></a></th>
         </tr>
     </thead>
     <tbody>
     @foreach($products as $product)
         <tr>
-            <td class="id" scope="row">{{$product['id']}}</td>
+            <td class="tbl_id" scope="row">{{$product['id']}}</td>
             <td class="product_name">{{$product['name']}}</td>
             <td class="category">{{$product['category_name']}}</td>
             <td class="brand">{{$product['brand']}}</td>
@@ -29,7 +47,7 @@
             <td class="price_core">{{$product['price_core']}}</td>
             <td class="price_sale">{{$product['price_sale']}}</td>
             <td class="note">{{$product['note']}}</td>
-            <td class="action">
+            <td class="tbl_action">
                 <a productID = "{{$product['id']}}" id="btn-delete" class="btn btn-danger btn-remove-row-1"><i class="fas fa-trash-alt" aria-hidden="true"></i></a>
                 <a href="{{ url('/product/'.$product['id'].'/edit') }}" class="btn btn-warning btn-update-row-1"><i class="fas fa-edit"></i></a>
                 {{--<a href="{{'http://localhost:8005/product/edit'}}" class="btn btn-warning btn-update-row-1"><i class="fas fa-edit"></i></a>--}}
@@ -38,8 +56,3 @@
     @endforeach
     </tbody>
 </table>
-<div class="col-md-12">
-    <div class="mt-15 panel-footer-search pull-right pagination_product" id="loadProduct">
-        {!!Paging::show($paging,0)!!}
-    </div>
-</div>
